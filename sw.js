da0ddd4d8d3913618bed45bf264f935018beade1
mnalyousif <mahmood@mahmood.tv>
2025-07-11 20:21:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('infant-tracker-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/legacy_data.json',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
