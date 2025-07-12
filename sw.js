ac6b377566ab2c2b5b549016c9901b91cdf50c7d
mnalyousif <mahmood@mahmood.tv>
2025-07-12 08:18:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,19 @@
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('infant-tracker-cache').then(function(cache) {
+    caches.open('infant-tracker-cache').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/legacy_data.json',
-        '/manifest.json'
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ]);
     })
   );
 });
-
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
